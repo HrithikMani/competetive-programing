ef36cefb45b337fdaed06970f7e09a99cf76bef5
Hrithik Mani <hrihtik.mani2000@gmail.com>
2021-05-06 01:44:20 +0530

    Memoization of knapsack

--- dp/KnapSack.java
+++ dp/KnapSack.java
@@ -1,21 +1,41 @@
 
 import java.math.*;
+import java.util.*;
+
 public class KnapSack {
-    public static int fun(int[] val,int[] wt,int n,int W){
+    public static void display(int[][] a){
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[0].length;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int fun(int[] val,int[] wt,int n,int W,int[][] dp){
+        display(dp);
         if(n==0 || W==0){
             return 0;
         }
+        if(dp[n][W] != 0){
+            return dp[n][W];
+        }
         if(wt[n-1] <= W){
-            return Math.max(val[n-1] + fun(val,wt,n-1,W - wt[n-1]),fun(val,wt,n-1,W));
+            dp[n][W] = Math.max(val[n-1] + fun(val,wt,n-1,W - wt[n-1],dp),fun(val,wt,n-1,W,dp));
+            return dp[n][W];
         }else{
-            return fun(val,wt,n-1,W);
+            dp[n][W] = fun(val,wt,n-1,W,dp);
+            return dp[n][W];
         }
     }
     public static void main(String args[]){
         int val[] = new int[]{60,100,120};
         int wt[] = new int[]{10,20,30};
-        int W=50;
+        int W=10;
         int n= val.length;
-        System.out.println(fun(val,wt,n,W));
+
+        int[][] dp =new int[n+1][W+1];
+
+        System.out.println(fun(val,wt,n,W,dp));
     }
 }
